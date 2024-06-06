29a2882b3b4ee1f77c461a0e599941d372d83dde
Matthew Nguyen <mnguy1231@gmail.com>
2024-06-06 12:55:07 -0700

    feat: Initial commit
    Start off with a simple text input & write to console the changes

---
+++ frontend/src/components/Type.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return {
+            typedText: ""
+        }
+    },
+    methods: {
+    },
+    watch: {
+        typedText(oldTypedText, newTypedText) {
+            // based on assumption that this is called on each char / each change
+            if (oldTypedText.length < newTypedText.length) {
+                console.log("+" + newTypedText.slice(-1))
+            } else {
+                console.log("-" + oldTypedText.slice(-1))
+            }
+        }
+    }
+}
+</script>
+<template>
+    <div> Hello world </div>
+    <input for="test" spellcheck="false" autocapitalize="off" autocomplete="off" v-model="typedText" placeholder="Type here">
+</template>
