3aeb58ee8ed93e318acc30e6cfa73424cfa9b94c
Matthew Nguyen <mnguy1231@gmail.com>
2024-06-12 15:50:53 -0700

    backend: Accept websocket connections from any origin

--- backend/client.go
+++ backend/client.go
@@ -9,6 +9,9 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 // acts as middleman between frontend websocket and a Lobby
