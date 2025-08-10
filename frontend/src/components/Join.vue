95afb61cd6da9fbd8a6144d71cb72619848b972c
Matthew Nguyen <mnguy1231@gmail.com>
2025-08-09 21:19:58 -0700

    chore: format vue files

--- frontend/src/components/Join.vue
+++ frontend/src/components/Join.vue
@@ -2,39 +2,39 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    data() {
-        return {
-            socket: null as WebSocket | null,
-            lobbyId: "",
-            username: "",
-            players: [],
-        }
-    },
-    mounted() {
-        this.socket = new WebSocket("ws://localhost:8080/ws");
-        this.socket.onopen = function(event: any) {
-            console.log("Connected to server", event);
-            this.send(JSON.stringify({"type": "LobbyInfo"}));
-        }
-        this.socket.onmessage = this.parseMessage;
-    },
-    methods: {
-        parseMessage(event: any) {
-            console.log("Received event", event);
-            let eventPayload = JSON.parse(event.data);
-            if (eventPayload["type"] == "LobbyInfo") {
-                this.lobbyId = eventPayload.data["lobbyId"];
-                this.username = eventPayload.data["username"];
-                this.players = eventPayload.data["players"];
-            }
-        }
-    },
+	data() {
+		return {
+			socket: null as WebSocket | null,
+			lobbyId: "",
+			username: "",
+			players: [],
+		}
+	},
+	mounted() {
+		this.socket = new WebSocket("ws://localhost:8080/ws");
+		this.socket.onopen = function (event: any) {
+			console.log("Connected to server", event);
+			this.send(JSON.stringify({ "type": "LobbyInfo" }));
+		}
+		this.socket.onmessage = this.parseMessage;
+	},
+	methods: {
+		parseMessage(event: any) {
+			console.log("Received event", event);
+			let eventPayload = JSON.parse(event.data);
+			if (eventPayload["type"] == "LobbyInfo") {
+				this.lobbyId = eventPayload.data["lobbyId"];
+				this.username = eventPayload.data["username"];
+				this.players = eventPayload.data["players"];
+			}
+		}
+	},
 })
 </script>
 
 <template>
-<p v-if="lobbyId">Lobby ID: {{ lobbyId }}</p>
-<p v-if="username">Username: {{ username }}</p>
-<p v-if="players">Players: {{ players }}</p>
+	<p v-if="lobbyId">Lobby ID: {{ lobbyId }}</p>
+	<p v-if="username">Username: {{ username }}</p>
+	<p v-if="players">Players: {{ players }}</p>
 
 </template>
--- frontend/src/components/Type.vue
+++ frontend/src/components/Type.vue
@@ -1,98 +1,98 @@
-
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 // -1 = correct 
-function firstDiffIndex(compare : string, truth : string) : number {
-    for (let i = 0; i < compare.length; i++) {
-        if (i > truth.length) {
-            return truth.length
-        }
+function firstDiffIndex(compare: string, truth: string): number {
+	for (let i = 0; i < compare.length; i++) {
+		if (i > truth.length) {
+			return truth.length
+		}
 
-        if (compare[i] != truth[i]) {
-            return i
-        }
-    }
+		if (compare[i] != truth[i]) {
+			return i
+		}
+	}
 
-    return -1
+	return -1
 }
 
 export default defineComponent({
-    data() {
-        return {
-            typedWord: "",
-            quote: "We don't understand what really causes events to happen. History is the fiction we invent to persuade ourselves that events are knowable and that life has order and direction. That's why events are always reinterpreted when values change. We need new versions of history to allow for our current prejudices.",
-            currentWordIndex: 0,
-        }
-    },
-    computed: {
-        quoteArray() : string[] {
-            return this.quote.split(/(?<=\s)/);
-        }
-    },
-    methods: {
-        quoteHtml() : string {
-            let correct : string = "";
-            if (this.currentWordIndex > 0) {
-                correct = "<span style=\"color: green\">" + this.quoteArray.slice(0, this.currentWordIndex).join("") + "</span>";
-            }
-            let htmlWord : string = "";
-            let currentWord : string = this.quoteArray[this.currentWordIndex];
+	data() {
+		return {
+			typedWord: "",
+			quote: "We don't understand what really causes events to happen. History is the fiction we invent to persuade ourselves that events are knowable and that life has order and direction. That's why events are always reinterpreted when values change. We need new versions of history to allow for our current prejudices.",
+			currentWordIndex: 0,
+		}
+	},
+	computed: {
+		quoteArray(): string[] {
+			return this.quote.split(/(?<=\s)/);
+		}
+	},
+	methods: {
+		quoteHtml(): string {
+			let correct: string = "";
+			if (this.currentWordIndex > 0) {
+				correct = "<span style=\"color: green\">" + this.quoteArray.slice(0, this.currentWordIndex).join("") + "</span>";
+			}
+			let htmlWord: string = "";
+			let currentWord: string = this.quoteArray[this.currentWordIndex];
 
-            let firstMistake : number = firstDiffIndex(this.typedWord, currentWord);
+			let firstMistake: number = firstDiffIndex(this.typedWord, currentWord);
 
-            if (firstMistake != -1) {
-                htmlWord += "<span style=\"color: green\">" + currentWord.slice(0, firstMistake) + "</span>" 
-                htmlWord += "<span style=\"background-color: red\">" 
-                
-                htmlWord += currentWord.slice(firstMistake, currentWord.length);
-                if (this.typedWord.length > currentWord.length) {
-                   htmlWord += this.typedWord.slice(currentWord.length, Math.min(15, this.typedWord.length));
-                }
-                htmlWord += "</span>";
-            } else {
-                htmlWord += "<span style=\"color: green\">" + this.typedWord + "</span>";
-                htmlWord += currentWord.slice(this.typedWord.length, currentWord.length);
-            }
+			if (firstMistake != -1) {
+				htmlWord += "<span style=\"color: green\">" + currentWord.slice(0, firstMistake) + "</span>"
+				htmlWord += "<span style=\"background-color: red\">"
+
+				htmlWord += currentWord.slice(firstMistake, currentWord.length);
+				if (this.typedWord.length > currentWord.length) {
+					htmlWord += this.typedWord.slice(currentWord.length, Math.min(15, this.typedWord.length));
+				}
+				htmlWord += "</span>";
+			} else {
+				htmlWord += "<span style=\"color: green\">" + this.typedWord + "</span>";
+				htmlWord += currentWord.slice(this.typedWord.length, currentWord.length);
+			}
 
 
-            let unfinished = this.quoteArray.slice(this.currentWordIndex+ 1, this.quoteArray.length).join("");
+			let unfinished = this.quoteArray.slice(this.currentWordIndex + 1, this.quoteArray.length).join("");
 
-            return correct + htmlWord + unfinished
-        }
-    },
-    watch: {
-        typedWord(newTypedWord: string, oldTypedWord: string) {
-            // based on assumption that this is called on each change
-            let diff : number = Math.abs(oldTypedWord.length - newTypedWord.length);
-            if (oldTypedWord.length < newTypedWord.length) {
-                console.log("+" + newTypedWord.slice(-diff));
-            } else {
-                console.log("-" + oldTypedWord.slice(-diff));
-            }
+			return correct + htmlWord + unfinished
+		}
+	},
+	watch: {
+		typedWord(newTypedWord: string, oldTypedWord: string) {
+			// based on assumption that this is called on each change
+			let diff: number = Math.abs(oldTypedWord.length - newTypedWord.length);
+			if (oldTypedWord.length < newTypedWord.length) {
+				console.log("+" + newTypedWord.slice(-diff));
+			} else {
+				console.log("-" + oldTypedWord.slice(-diff));
+			}
 
-            if (newTypedWord == this.quoteArray[this.currentWordIndex])  {
-                this.typedWord = "";
-                this.currentWordIndex++;
-                console.log("Finished word " + newTypedWord);
+			if (newTypedWord == this.quoteArray[this.currentWordIndex]) {
+				this.typedWord = "";
+				this.currentWordIndex++;
+				console.log("Finished word " + newTypedWord);
 
-                if (this.currentWordIndex == this.quoteArray.length) {
-                    alert("You finished the quote!");
-                }
-            }
-        }
-    }
+				if (this.currentWordIndex == this.quoteArray.length) {
+					alert("You finished the quote!");
+				}
+			}
+		}
+	}
 })
 
 </script>
 <template>
-    <section class="">
-        <blockquote class="block is-size-4-desktop">
-            <p v-html="quoteHtml()"></p>
-            <footer>
-                <cite>- author</cite>
-            </footer>
-        </blockquote>
-        <input class="block input" type="text" for="text" spellcheck="false" autocapitalize="off" autocomplete="off" v-model="typedWord" placeholder="Type here">
-    </section>
+	<section class="">
+		<blockquote class="block is-size-4-desktop">
+			<p v-html="quoteHtml()"></p>
+			<footer>
+				<cite>- author</cite>
+			</footer>
+		</blockquote>
+		<input class="block input" type="text" for="text" spellcheck="false" autocapitalize="off"
+			autocomplete="off" v-model="typedWord" placeholder="Type here">
+	</section>
 </template>
